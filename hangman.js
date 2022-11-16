5ad48b32609f3607b7304fd7d4d84b711ca5b4cb
mpsinghk <mpsinghk@users.noreply.github.com>
2022-11-15 21:50:49 -0800

    Update 2022-11-15

--- hangman.js
+++ hangman.js
@@ -1,16 +1,43 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+const maxGuesses = 10;
+let currentGuesses;
+
+function printGuess(word) {
+    const randomWordEl = document.querySelector('.random-word');
+
+    for (let i = 0; i < word.length; i++) {
+        randomWordEl.textContent += '_';
+        // randomWordEl.textContent += word[i].toUpperCase();
+    }
+}
+
+function keyClickHandler(guessedLetter) {
+    const guessedKeyEl = document.querySelector(`.key-${guessedLetter}`);
+
+    guessedKeyEl.setAttribute('disabled', '');
+    console.log(guessedLetter);
+}
 
 function generateRandomWord() {
-    const randomWordEl = document.getElementById('radom-word');
+    const words = "The light blinded him. It was dark and he thought he was the only one in the area. The light shining in his eyes proved him wrong. It came from about hundred feet away and was shining so directly into his eyes he could not make out anything about the person holding the light. There was only one thing to do in this situation. He reached into his pocket and pulled out the flashlight of his own that was much stronger than the one currently blinding him. He turned it on and pointed it forward.".replaceAll('.', '').split(' ');
+    const uniqueWords = [...new Set(words)];
+    const randomWord = uniqueWords[Math.floor(Math.random() * uniqueWords.length)];
+
+    printGuess(randomWord);
+    console.log(randomWord);
 }
 
 function generateKeypad() {
-    const keypadEl = document.getElementById('keypad');
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const keypadEl = document.querySelector('.keypad');
+
     alphabet.forEach(letter => {
-        const key = document.createElement('button');
-        key.classList.add('key', `key-${letter.toLowerCase()}`);
-        key.textContent = letter;
-        keypadEl.appendChild(key);
+        const keyEl = document.createElement('button');
+        keyEl.classList.add('key', `key-${letter}`);
+        keyEl.textContent = letter.toUpperCase();
+        keypadEl.appendChild(keyEl);
+
+        // keyEl.setAttribute('onclick', `keyClickHandler('${letter}')`);
+        keyEl.addEventListener('click', () => keyClickHandler(`${letter}`));
     });
 }
 
